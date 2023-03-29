52a9002ac32f9d794aefd0825df361e9c315cf53
BuildTools <unconfigured@null.spigotmc.org>
2023-03-29 20:53:40 +1100

    Formatted

--- components/turret.py
+++ components/turret.py
@@ -5,33 +5,41 @@ from wpimath.controller import ProfiledPIDControllerRadians
 from wpimath.trajectory import TrapezoidProfileRadians
 from ids import SparkMaxIds, DioChannels
 
-from  magicbot import tunable, feedback
+from magicbot import tunable, feedback
 
 
-GEAR_RATIO: float = 18/140
+GEAR_RATIO: float = 18 / 140
 ANGLE_ERROR_TOLERANCE: float = radians(1)
 MAX_ANGULAR_VELOCITY: float = 1.0
 MAX_ANGULAR_ACCELERATION: float = 0.5
 NEGATIVE_LIMIT_ANGLE: float = radians(-120)
 POSITIVE_LIMIT_ANGLE: float = radians(120)
-INDEX_SEARCH_VOLTAGE:float = 2.0
+INDEX_SEARCH_VOLTAGE: float = 2.0
+
 
 class Turret:
-    
     goal_angle = tunable(0.0)
 
     def __init__(self) -> None:
         # Create the hardware object handles
-        self.motor = CANSparkMax(SparkMaxIds.turret_motor, CANSparkMax.MotorType.kBrushless) #This will need to change to the proper motor controller type
+        self.motor = CANSparkMax(
+            SparkMaxIds.turret_motor, CANSparkMax.MotorType.kBrushless
+        )  # This will need to change to the proper motor controller type
         self.motor.setIdleMode(CANSparkMax.IdleMode.kBrake)
         self.motor.setInverted(False)
         self.positive_limit_switch = DigitalInput(DioChannels.positive_turret_switch)
-        self.negative_limit_switch= DigitalInput(DioChannels.negative_turret_switch) # These could also be attached directly to the motor controller breakout board for interrupts
+        self.negative_limit_switch = DigitalInput(
+            DioChannels.negative_turret_switch
+        )  # These could also be attached directly to the motor controller breakout board for interrupts
         self.encoder = self.motor.getEncoder()
         self.encoder.setPositionConversionFactor(GEAR_RATIO)
         self.encoder.setVelocityConversionFactor(GEAR_RATIO)
-        rotation_contraints = TrapezoidProfileRadians.Constraints(maxVelocity=MAX_ANGULAR_VELOCITY,maxAcceleration=MAX_ANGULAR_ACCELERATION)
-        self.rotation_controller = ProfiledPIDControllerRadians(2,0.0,0.0,rotation_contraints)
+        rotation_contraints = TrapezoidProfileRadians.Constraints(
+            maxVelocity=MAX_ANGULAR_VELOCITY, maxAcceleration=MAX_ANGULAR_ACCELERATION
+        )
+        self.rotation_controller = ProfiledPIDControllerRadians(
+            2, 0.0, 0.0, rotation_contraints
+        )
         # set index found var to false
         self.index_found = False
 
@@ -39,7 +47,7 @@ class Turret:
         # set the desired angle for the turret
         clamped_angle = min(angle, POSITIVE_LIMIT_ANGLE - radians(5))
         clamped_angle = max(clamped_angle, NEGATIVE_LIMIT_ANGLE + radians(5))
-        self.goal_angle = clamped_angle        
+        self.goal_angle = clamped_angle
 
     @feedback
     def get_angle(self) -> float:
@@ -54,7 +62,7 @@ class Turret:
     @feedback
     def at_positive_limit(self) -> bool:
         return not self.positive_limit_switch.get()
-    
+
     @feedback
     def at_negative_limit(self) -> bool:
         return not self.negative_limit_switch.get()
@@ -66,26 +74,20 @@ class Turret:
         if self.at_negative_limit():
             self.encoder.setPosition(NEGATIVE_LIMIT_ANGLE)
             self.index_found = True
-            
 
         if self.at_positive_limit():
             self.encoder.setPosition(POSITIVE_LIMIT_ANGLE)
             self.index_found = True
 
-        
         if self.index_found:
             # calculate pid output based off angle delta
-            pid_output = self.rotation_controller.calculate(self.get_angle(), self.goal_angle)
+            pid_output = self.rotation_controller.calculate(
+                self.get_angle(), self.goal_angle
+            )
             # calcualte feed forward based off angle delta
             # clamp input
-            
+
             self.motor.setVoltage(pid_output)
         else:
-        # set desired output voltage or speed to find index
+            # set desired output voltage or speed to find index
             self.motor.setVoltage(INDEX_SEARCH_VOLTAGE)
-
-
-       
-        
-
-        
