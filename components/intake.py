e182ca73514b5dd51c0feed95e8162ad8693c25f
David Vo <auscompgeek@users.noreply.github.com>
2023-04-16 17:59:46 +1000

    intake: Add tunnel motor

--- components/intake.py
+++ components/intake.py
@@ -1,3 +1,4 @@
+from ctre import WPI_TalonSRX
 from magicbot import tunable
 from rev import CANSparkMax
 from wpilib import DoubleSolenoid, PneumaticsModuleType
@@ -7,6 +8,7 @@ import ids
 
 class Intake:
     front_speed = tunable(0.8)
+    tunnel_speed = tunable(0.8)
     deployed = tunable(False)
 
     def __init__(self) -> None:
@@ -16,7 +18,9 @@ class Intake:
         self.front_motor.restoreFactoryDefaults()
         self.front_motor.setSmartCurrentLimit(30)
         self.front_motor.setInverted(True)
-        # TODO: tunnel_motor
+
+        self.tunnel_motor = WPI_TalonSRX(ids.TalonIds.intake_tunnel)
+
         self.piston = DoubleSolenoid(
             PneumaticsModuleType.REVPH,
             ids.PhChannels.intake_piston_forward,
@@ -33,6 +37,8 @@ class Intake:
         if self.deployed:
             self.piston.set(DoubleSolenoid.Value.kForward)
             self.front_motor.set(self.front_speed)
+            self.tunnel_motor.set(self.tunnel_speed)
         else:
             self.piston.set(DoubleSolenoid.Value.kReverse)
             self.front_motor.stopMotor()
+            self.tunnel_motor.stopMotor()
