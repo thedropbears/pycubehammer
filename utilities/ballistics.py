b3de543c101cfbc1f995c52ecc9b408fc27b7387
James Ward <james@thedropbears.org.au>
2023-04-16 12:27:34 +1000

    Do calculations for turret angle

--- utilities/ballistics.py
+++ utilities/ballistics.py
@@ -4,6 +4,8 @@ from enum import Enum
 
 from wpimath.geometry import Pose2d
 
+from utilities.functions import constrain_angle
+
 
 class GoalHeight(Enum):
     HIGH = 2
@@ -22,5 +24,12 @@ class BallisticsSolution:
 def calculate_ballistics(
     robot_pose: Pose2d, target_pose: Pose2d, target_height: GoalHeight
 ) -> BallisticsSolution:
-    bs = BallisticsSolution(math.radians(90), math.radians(45), 300, 300)
+    azimuth = math.atan2(target_pose.y - robot_pose.y, target_pose.x - robot_pose.x)
+    turret_angle = constrain_angle(azimuth - robot_pose.rotation().radians())
+    bs = BallisticsSolution(
+        turret_angle=turret_angle,
+        tilt_angle=math.radians(45),
+        top_flywheel_speed=300,
+        bottom_flywheel_speed=300,
+    )
     return bs
